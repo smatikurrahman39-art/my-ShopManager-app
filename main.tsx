f3842be55e2e23079be31fa9f6ce73f31ff548ba
smatikurrahman39-art <smatikurrahman39@gmail.com>
2026-04-17 09:25:51 +0600

    Add service worker registration logic
    Register service worker on window load if supported.

--- main.tsx
+++ main.tsx
@@ -11,3 +11,10 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 );
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('SW registered!', reg))
+      .catch(err => console.log('SW registration failed!', err));
+  });
+}
